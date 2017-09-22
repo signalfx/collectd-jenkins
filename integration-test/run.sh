12b1e09b43911fc1af9eba84ae750e773d318b31
Charlie Smith <charlie@signalfx.com>
2017-09-22 15:13:00 -0400

    fix integration tests

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -4,6 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
+cp Dockerfile.jenkins Dockerfile.jenkins.2603
+sed -i -e "s#%%%VER%%%#2.60.3#g" ./Dockerfile.jenkins.2603
+cp Dockerfile.jenkins Dockerfile.jenkins.2463
+sed -i -e "s#%%%VER%%%#2.46.3#g" ./Dockerfile.jenkins.2463
+
 printf "Running tests against python2.7\n"
 docker-compose run --rm test
 status=$?
